3687c0088b94d324e3963a49595e9eec8b7e1c37
Rontia <ronitatembo@gmail.com>
2022-09-08 16:12:49 -0700

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#!/bin/bash
+#include <stdio.h>
+
+#include <stdio.h>
+
+
+
+/**
+ *main - printing
+ *
+ * return: Always 0 (Successful)
+ */
+
+int main(void)
+
+{
+     puts("\ ""Programming is like building a multilingual puzzle"); 
+	    return (0);
+
+}
