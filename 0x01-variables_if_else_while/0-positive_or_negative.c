1e5220a26e2265b3383123ae22620488ce04e053
Rontia <ronitatembo@gmail.com>
2022-09-09 13:49:51 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#!/bin/bash
+/*
+ * File: 0-positive_or_negative.c
+ *
+ */
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/*
+ *  main - prints a random number and states whether 
+ *  it is positive, negative, or zero.
+ *
+ *  Return: Always 0.
+ */
+int main(void)
+{ 
+	int n;
+
+	srand(time(0));
+	n = rand () - RAND_MAX / 2;
+
+	if(n > 0)
+		print("%d is positive\n", n);
+	else if(n < 0)
+		print("%d is negative\n", n);
+	else
+		print("%d is zero\n", n);
+	return(0);
+
+}
