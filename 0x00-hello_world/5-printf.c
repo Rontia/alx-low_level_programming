185bfe7208cbcdb2712406562bbb4822c80c3207
Rontia <ronitatembo@gmail.com>
2022-09-11 14:36:14 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main prints a text
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n")
+		retun(0);
+}
