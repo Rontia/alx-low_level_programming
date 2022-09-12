5e034efa901fb03177802ceec3936b65cf41a446
Rontia <ronitatembo@gmail.com>
2022-09-12 14:35:30 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
 
 /**
- * main prints a text
- *
- * Return: Always 0 (Success)
- */
+* main - prints a text
+*
+* Description: Printing with proper grammar
+* Return: Always 0 (Success)
+*/
 int main(void)
-{
-	printf("with proper grammer, but the outcome is a piece of art,\n")
-		retun(0);
+{printf("with proper grammer, but the outcome is a piece of art,\n");
+retun (0);
 }
