d5055760fa8b94a873fe7d8922871125c9b2ef41
Rontia <ronitatembo@gmail.com>
2022-09-12 14:27:41 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- * main - printing 
- *
- * Description: using the main function
- * this program prints programming is like building a multIlingual puzzle
- * Return: 0
- */
+* main - printing 
+*
+* Description: using the main function
+* this program prints programming is like building a multIlingual puzzle
+* Return: 0
+*/
 int main(void)
 {puts("\"Programming is like building a multilingual puzzle");
 return (0);
