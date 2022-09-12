a9d56f22200a00e13de65e95126294d17196e498
Rontia <ronitatembo@gmail.com>
2022-09-12 15:45:25 -0700

    with proper grammar, but the outcome is a piece of art,

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+* main - print a string in the put function
+*
+* Description: using the main function
+* this program prints "programming is like building a multilingual piece of art
+* Return: 0
+*/
+int main(void)
+{printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
