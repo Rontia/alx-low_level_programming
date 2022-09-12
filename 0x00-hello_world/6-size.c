040908d61cc558e377d847eb9257c3510a72ff7b
Rontia <ronitatembo@gmail.com>
2022-09-12 15:17:05 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+* main - prints a text
+*
+* Description: using the main function
+* this program is printing"just like the previous one as a build-up
+* Return: 0
+*/
+int main(void)
+{char c;
+int a;
+long la;
+long long lla;
+float b;
+printf("Size of a char: %ld byte(s)\n", sizeof(c));
+printf("Size of an int: %ld byte(s)\n", sizeof(a));
+printf("Size of a long int: %ld byte(s)\n", sizeof(la));
+printf("Size of a long long int: %ld byte(s)\n", sizeof(lla));
+printf("Size of a float: %ld byte(s)\n", sizeof(c));
+return (0);
+}
