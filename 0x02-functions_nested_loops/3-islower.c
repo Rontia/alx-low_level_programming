4adc7d7314947b8b71b248016c3d926f20d4c17d
Rontia <ronitatembo@gmail.com>
2022-09-14 09:41:03 -0700

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -7,23 +7,13 @@
 * @c: is the int that will use for the argument of the function
 *
 * Return: 0
-*
 */
-
 int _islower(int c)
-
 {
-
 if (c >= 'a' && c <= 'z')
-
 {
-
 return (1);
-
 }
-
 else
-
 return (0);
-
 }
