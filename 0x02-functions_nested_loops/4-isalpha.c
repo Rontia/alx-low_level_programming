fbadd077a94f1864da4e5c1f789383e19d4c30db
Rontia <ronitatembo@gmail.com>
2022-09-14 13:11:20 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,7 +2,7 @@
 /**
 * isalpha - function to check if c is a letter , lowercase or uppercase
 * @c: is the int that will use the argument of the function
-* Reurn: 0
+* Return: 0
 */
 int _isalpha(int c)
 {
