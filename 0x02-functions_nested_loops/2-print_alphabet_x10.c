fbe80e58cd80481ba4b0fd016650edb22d084560
Rontia <ronitatembo@gmail.com>
2022-09-14 15:22:18 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+* print_alphabet_x10 - function to print abc 10 times
+* Retutn: 0
+*/
+void print_alphabet_x10(void)
+{
+char c, i;
+for (i = 0; i <=9; i++)
+{
+(c = 'a'; c <= 'z'; c++)
+for
+{
+_putchar(c);
+}
+_putchar('\n');
+}
+}
