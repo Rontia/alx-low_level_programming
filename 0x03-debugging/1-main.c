4025eeec9496f589c7d7df90c103c645343dff0d
Rontia <ronitatembo@gmail.com>
2022-09-15 16:39:11 -0700

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -7,13 +7,15 @@ int main(void)
 {
 int i;
 printf("infinite loop incoming :(\n");
+
 i = 0;
 /**
-* while (i < 0)
-* {
-* putchar(i);
-* }
+*while (i < 0)
+*{
+*putchar(i);
+*}
 */
 printf("infinite loop avoided! \\o/\n");
+
 return (0);
 }
