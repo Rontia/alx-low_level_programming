6d1fcd51c34e4635c2a09ae43acc9b67b4ffcc39
Rontia <ronitatembo@gmail.com>
2022-09-15 18:12:53 -0700

    2-largest_number.c

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -11,17 +11,21 @@
 int largest_number(int a, int b, int c)
 {
 int largest;
+
 if (a > b && a > c)
 {
 largest = a;
 }
-if(b > c)
+
+else if (b > c)
 {
 largest = b;
 }
+
 else
 {
 largest = c;
 }
+
 return (largest);
 }
