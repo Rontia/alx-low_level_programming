07a611b630a9df34cd24f3536c66abd48b5a6b49
Rontia <ronitatembo@gmail.com>
2022-09-16 19:10:27 -0700

    10-print_triangle.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,17 +2,17 @@
 #include "main.h"
 
 /**
-* mul - multiplies two integers
-*
-* @a: first input
-* @b: second input
-*
-* Return: product of a & b
-*/
+ * mul -multiplies two integers
+ *
+ * @a: first input
+ * @b: second input
+ *
+ * Return: product of a & b
+ */
 
 int mul(int a, int b)
 {
-int product = a * b;
+int product == a * b;
 
 return (product);
 }
