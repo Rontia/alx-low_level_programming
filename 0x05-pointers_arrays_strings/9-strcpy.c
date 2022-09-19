39018153ee61e944dbf3f5688138751ead0ee47f
Rontia <ronitatembo@gmail.com>
2022-09-19 15:26:33 -0700

    9-strcpy.c

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_array - prints n digit of an array
+ *@a: array
+ *@n: number of values
+ *Return: returns a and n inputs
+*/
+char *_strcpy(char *dest, char *src)
+{
+int s;
+
+for (s = 0; s < n; s++)
+{
+printf("%d", a[s]);
+if (s != n - 1)
+printf(", ")
+}
+printf('\n');
+}
