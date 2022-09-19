eb9a31730377321a9ab384574d290595606a6b1e
Rontia <ronitatembo@gmail.com>
2022-09-19 14:30:45 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,11 @@
  */
 void print_rev(char *s)
 {
-	int i =0;
+	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
-	for(i = i - 1; i <= 0; i--)
+	for (i = i - 1; i <= 0; i--)
 		_putchar(s[i]);
 
 	_putchar('\n');
