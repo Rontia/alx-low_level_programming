96262b7fcf6ca5e2e62dd5b8dd2302dafa967184
Rontia <ronitatembo@gmail.com>
2022-09-21 16:11:25 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,14 +10,16 @@ char *cap_string(char *)
 {
 int index = 0;
 
-while (str++[index])
+while (str[++index])
 {
 while (!(str[index] >= 'a') && (str[index] <= 'z'))
 index++;
+
 if (str[index - 1] == ' ' ||
 str[index - 1] == '\t' ||
 str[index - 1] == '\n' ||
 str[index - 1] == ',' ||
+str[index - 1] == ';' ||
 str[index - 1] == '.' ||
 str[index - 1] == '!' ||
 str[index - 1] == '?' ||
