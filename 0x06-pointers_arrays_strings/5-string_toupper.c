a1043daa82b7bf15a0149cc4d8228e5fae4ff0dc
Rontia <ronitatembo@gmail.com>
2022-09-21 15:39:17 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,9 +2,9 @@
 
 /**
  * string_toupper - changes all lowercase letters to uppercase
- * @str: string to be changed
+ * @s: string
  *
- * Return: returns address to the string
+ * Return: s
  */
 
 char *string_toupper(char *)
@@ -13,7 +13,7 @@ int x;
 
 for (x = 0; s[x] != '\0'; x++)
 {
-if (s[x] > 96 && s[x] < 125]
+if (s[x] > 96 && s[x] < 125)
 {
 s[x] -= 32;
 }
