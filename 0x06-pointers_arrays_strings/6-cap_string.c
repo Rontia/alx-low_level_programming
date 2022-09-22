64827cef6d59e3d3c5d2a1037244378a6ddcbb73
Rontia <ronitatembo@gmail.com>
2022-09-22 13:15:00 -0700

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
 /**
-* cap_string - function that capitalises the first character of a word
-* @s: string to capitalize
-* Return: address of s
-*/
-
-char *cap_string(char *)
+ * cap_string - capitalizes all words in a string
+ * @s: string to capitalize
+ * Return: address of s
+ */
+char *cap_string(char *s)
 {
+
 int i = 0, j;
 char a[] = " \t\n,;.!?\"(){}";
 
 while (*(s + i))
+
 {
 if (*(s + i) >= 'a' && *(s + i) <= 'z')
 {
@@ -19,7 +20,7 @@ if (i == 0)
 *(s + i) -= 'a' - 'A';
 else
 {
-for (j = 0; j < 12; j++)
+for (j = 0; j <= 12; j++)
 {
 if (a[j] == *(s + i - 1))
 *(s + i) -= 'a' - 'A';
